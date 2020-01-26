1654d004725592749b61d06398c2a2a59c6cfd67
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-26 01:45:09 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,23 +2,19 @@ class Pokemon
   
   attr_accessor :id, :name, :type, :db, :hp
   
-  @@all = []
-  
+
   def initialize(id, name, type, db)
     @id = id 
     @name = name 
     @db = db
     @type = type
     
-    @@all << self
   end 
   
   def self.save
-    @@all << self
   end
   
   def self.find 
-    @@all.detect()
   end 
   
   
