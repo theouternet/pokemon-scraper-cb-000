73571b69b72012de908470243ee6c78f0b2a866f
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-26 01:40:11 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,18 @@
 class Pokemon
+  
+  @@all = []
+  
+  def initialize(id, name, type, db)
+    @id = id 
+    @name = name 
+    @db = db
+    @type = type
+    
+    @@all << self
+    
+  end 
+  
+  def self.save
+    @@all << self
+  
 end
