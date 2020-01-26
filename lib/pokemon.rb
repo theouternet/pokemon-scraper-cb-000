b4edb5205fcc7f4b7ed5e0b6a5aefd52413c32a5
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-26 03:05:59 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,11 +11,11 @@ class Pokemon
     
   end 
   
-  def self.save(name, type)
+  def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
   end
   
-  def self.find(name, type, db="nil")
+  def self.find(name, type, db)
     db.
   end 
   
